b1dd06268243d60833d0af208fc6614a5dfd6f69
Marc Nitzsche <marc.nitzsche@live.de>
2015-04-25 21:30:31 +0200

    Readd the word-checker

--- PygLatin.py
+++ PygLatin.py
@@ -1,6 +1,6 @@
 print "Welcome to PigLatin Translator 0.1!"
 print ""
-pyg = 'ay' # Define suffix
+suffix = 'ay' # Define suffix
 def getInput():
     return raw_input("Enter your word:") # Use it for everything
 
@@ -9,24 +9,30 @@ def firstUp(original, word):
         return word[0].upper() + word[1:] # Makes The First Letter Upper ;)
     else:
         return word
-
+def isItAWord(word):
+    if len(word) > 0 and word.isalpha(): # Check for common mistakes in writing
+        return word
+    else:
+        return "Error: No Word! (Error Code: 0x589dfff)"
 def toEng():
     original = getInput()
     word = original[:len(original)-2].lower() # Get word without suffix + lowercase
     firstLetter = word[len(word)-1] # Get (the real) first letter
     word = firstLetter + word[:len(word)-1]# First letter + word - first letter - suffix
     piggy = firstUp(original, word) # First letter to upper?
-    return piggy
+    return isItAWord(piggy)
 
 def toPyg():
-    original = getInput() # Original to Lower
-    word = original+original[0].lower()+pyg # Define Base + first to lowercase
+    original = isItAWord(getInput())
+    word = original+original[0].lower()+suffix # Define Base + first to lowercase
     word = word[1:len(word)] # Get rid of the first first letter
     piggy = firstUp(original, word) # First letter to upper?
-    return piggy
+    return isItAWord(piggy)
 
-whatToDo = raw_input("1 or 2?")
+whatToDo = raw_input("Do you want to translate from PygLatin to English (1) or from English to PygLatin (2)?") # What should I do?!?
 if whatToDo == "1":
         print toEng()
 elif whatToDo == "2":
         print toPyg()
+else:
+    print "Error: There is no other choice! (Error Code: 0x132dfaa)"
