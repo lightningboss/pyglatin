16fc734bc8fb23de398bf5ff85e225678e72b6be
Marc Nitzsche <marc.nitzsche@live.de>
2015-04-25 21:29:57 +0200

    Restructure Code, add first-letter-check

--- PygLatin.py
+++ PygLatin.py
@@ -1,26 +1,32 @@
+print "Welcome to PigLatin Translator 0.1!"
+print ""
 pyg = 'ay' # Define suffix
-whatToDo = raw_input("Do you want to translate from PygLatin to English (1) or from English to PygLatin (2)?") # What should I do?!?
+def getInput():
+    return raw_input("Enter your word:") # Use it for everything
+
+def firstUp(original, word):
+    if original[0].isupper(): # Check whether first letter is uppercase
+        return word[0].upper() + word[1:] # Makes The First Letter Upper ;)
+    else:
+        return word
+
+def toEng():
+    original = getInput()
+    word = original[:len(original)-2].lower() # Get word without suffix + lowercase
+    firstLetter = word[len(word)-1] # Get (the real) first letter
+    word = firstLetter + word[:len(word)-1]# First letter + word - first letter - suffix
+    piggy = firstUp(original, word) # First letter to upper?
+    return piggy
+
+def toPyg():
+    original = getInput() # Original to Lower
+    word = original+original[0].lower()+pyg # Define Base + first to lowercase
+    word = word[1:len(word)] # Get rid of the first first letter
+    piggy = firstUp(original, word) # First letter to upper?
+    return piggy
+
+whatToDo = raw_input("1 or 2?")
 if whatToDo == "1":
-    original_1 = raw_input('Enter your word:') # Get input
-    if len(original_1) > 0 and original_1.isalpha():
-        wordLower = original_1.lower() # To lowercase
-        withoutAy = wordLower[:len(wordLower)-2] # Get word without -ay
-        firstLetter = withoutAy[len(withoutAy)-1] # Get first letter
-        final_word = firstLetter + withoutAy[:len(withoutAy)-1]# First letter + withoutAy - first leter
-        final_word = final_word[0].upper() + final_word[1:] # First letter to upper
-        print final_word
-    else:
-        print 'Error: empty (Error Code: 0x589dfff)'
+        print toEng()
 elif whatToDo == "2":
-    original_2 = raw_input('Enter your word:') # Get input
-    if len(original_2) > 0 and original_2.isalpha():
-        word = original_2.lower() # Original to Lower
-        first = word[0]
-        new_word = word+first+pyg # Define Base
-        new_word = new_word[1:len(new_word)] # Get rid of one first letter
-        new_word = new_word[0].upper() + new_word[1:] # First letter to upper
-        print new_word
-    else:
-        print 'Error: empty (Error Code: 0x589dfff)'
-else:
-    print "You didn't type in 1 or 2!"
+        print toPyg()
